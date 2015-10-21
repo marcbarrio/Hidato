6822c2b94da8e4b9ddf49e1b3f4057858abd1916
RaulPROP <raul_ibpe@hotmail.com>
2015-10-21 09:36:48 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * @author Hamid Latif
+ */
+//Clase main
+import Presentacion.ControladorPresentacion;
+
+public class Main {
+	public static void main(String[] args) {
+		ControladorPresentacion.getInstance().iniciar();
+	}
+}
