5441dc861ec3ad046d6407806426344caa0e5a52
RaulPROP <raul_ibpe@hotmail.com>
2015-10-21 09:43:29 +0200

    Create Partida.java

---
+++ Dominio/Partida/Partida.java
@@ -0,0 +1,18 @@
+/**
+ *
+ * @author Hamid Latif
+ */
+package Dominio.Partida;
+//import Dominio.Tiempo;
+
+public class Partida {
+    private int puntuacion;
+    private String nombreTablero = new String();
+    private String nombreJugador = new String();
+    private boolean invitado;
+    
+    
+    public void iniciar_invitado() {
+        
+    }
+}
