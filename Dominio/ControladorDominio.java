f38dde4bd93e89bb5048d4b146e1f76eff95e173
Raúl Ibáñez <raul_ibpe@hotmail.com>
2015-10-21 21:20:03 +0200

    Update ControladorDominio.java

--- Dominio/ControladorDominio.java
+++ Dominio/ControladorDominio.java
@@ -1,12 +1,16 @@
 /**
  *
- * @author Hamid Latif
+ * @author Raúl Ibáñez Pérez
  */
 package Dominio;
+import java.util.*;
 import Persistencia.ControladorPersistencia;
+import Dominio.Estructuras.Usuario;
+import Dominio.Estructuras.Tablero;
 
 public class ControladorDominio {
-    ControladorPersistencia Persistencia = new ControladorPersistencia();
+    ControladorPersistencia CPersistencia = new ControladorPersistencia();
+    Usuario usuario = new Usuario();
     
     /*  PRE: -
         POST: La clase queda inicializada
@@ -17,6 +21,50 @@ public class ControladorDominio {
     POST: Se retorna true si eltablero es válido y falsosiel tablero es inválido
     */
     public boolean inciar_sesion(String nombre, String contrasena) {
-        return Persistencia.iniciar_sesion(nombre, contrasena);
+        return CPersistencia.iniciar_sesion(nombre, contrasena);
     }
+    
+    public boolean nombreTableroValido(String s){
+        return CPersistencia.nombreTableroValido(s);
+    }
+    
+    public boolean nombreValido(String s){
+        return (s.length() > 5 && (CPersistencia.nombreExistente(s) == false));
+    }
+    
+    public boolean contrasenaValida(String c){
+        return (c.length() < 6);
+    }
+    
+    /* Pre: n es un nombre de usuario valido y c es una contraseña valida.
+    */
+    public void crearUsuario(String n, String c){
+        CPersistencia.crearUsuario(n,c);
+    }
+    
+    public boolean nombreExistente(String n){
+        return CPersistencia.nombreExistente(n);
+    }
+    
+    public boolean contrasenaValida(String n, String c){
+        return CPersistencia.contrasenaValida(n,c);
+    }
+    
+    public void escribirBDUsuarios(){
+        CPersistencia.crearBDUsuarios();
+    }
+    
+    /* PRE: n es un nombre de usuario válido y c es su contraseña
+    
+    */
+    public void loginUsuario(String n, String c){
+        usuario.setNombre(n);
+        usuario.setContrasena(c);
+        usuario.setPuntuacionTotal(CPersistencia.cargarPuntuacionUsuario(n));
+    }
+    
+    public int getPuntuacionUsuario(){
+        return usuario.getPuntuacionTotal();
+    }
+    
 }
