8b751e15ba84f282854a8d5b5fd6d7e2558281b9
Raúl Ibáñez <raul_ibpe@hotmail.com>
2015-10-23 11:57:23 +0200

    Update ControladorDominio.java

--- Dominio/ControladorDominio.java
+++ Dominio/ControladorDominio.java
@@ -5,12 +5,12 @@
 package Dominio;
 import java.util.*;
 import Persistencia.ControladorPersistencia;
-import Dominio.Estructuras.Usuario;
+import Dominio.Estructuras.UsuarioHidato;
 import Dominio.Estructuras.Tablero;
 
 public class ControladorDominio {
     ControladorPersistencia CPersistencia = new ControladorPersistencia();
-    Usuario usuario = new Usuario();
+    UsuarioHidato usuario = new UsuarioHidato();
     
     /*  PRE: -
         POST: La clase queda inicializada
@@ -29,7 +29,7 @@ public class ControladorDominio {
     }
     
     public boolean nombreValido(String s){
-        return (s.length() > 5 && (CPersistencia.nombreExistente(s) == false));
+        return (s.length() > 5 && (CPersistencia.nombreUsuarioExistente(s) == false));
     }
     
     public boolean contrasenaValida(String c){
@@ -40,14 +40,15 @@ public class ControladorDominio {
     */
     public void crearUsuario(String n, String c){
         CPersistencia.crearUsuario(n,c);
-        this.usuario = new Usuario(n,c);
+        String auxNombre = this.usuario.getNombre();
         int auxPuntuacion = this.usuario.getPuntuacionTotal();
         boolean auxRanking = this.usuario.getApareceRanking();
-        CPersistencia.crearBDUsuario(n, auxPuntuacion, auxRanking);
+        Date auxFecha = this.usuario.getFecha();
+        CPersistencia.guardarUsuario(n, auxPuntuacion, auxRanking, auxFecha);
     }
     
     public boolean nombreExistente(String n){
-        return CPersistencia.nombreExistente(n);
+        return CPersistencia.nombreUsuarioExistente(n);
     }
     
     public boolean contrasenaValida(String n, String c){
@@ -58,15 +59,20 @@ public class ControladorDominio {
     
     */
     public void loginUsuario(String n, String c){
-        this.usuario = new Usuario();
+        this.usuario = new UsuarioHidato();
         this.usuario.setNombre(n);
         this.usuario.setContrasena(c);
         this.usuario.setPuntuacionTotal(CPersistencia.cargarPuntuacionUsuario(n));
         this.usuario.setApareceRanking(CPersistencia.cargarApareceRankingUsuario(n));
+        this.usuario.setFecha(CPersistencia.cargarFechaUsuario(n));
     }
     
     public int getPuntuacionUsuario(){
-        return usuario.getPuntuacionTotal();
+        return this.usuario.getPuntuacionTotal();
+    }
+    
+    public Date getFechaCreacion(){
+        return this.usuario.getFecha();
     }
     
     public boolean getApareceRanking(){
@@ -90,9 +96,6 @@ public class ControladorDominio {
     /* PRE: el usuario debe estar inicializado.
     
     */
-    public void noAparecerRanking(){
-        
-    }
     
     public void jugarInvitado(){
         
@@ -107,7 +110,8 @@ public class ControladorDominio {
         String auxNombre = this.usuario.getNombre();
         int auxPuntuacion = this.usuario.getPuntuacionTotal();
         boolean auxRanking = this.usuario.getApareceRanking();
-        CPersistencia.guardarUsuario(auxNombre,auxPuntuacion,auxRanking);
+        Date auxFecha = this.usuario.getFecha();
+        CPersistencia.guardarUsuario(auxNombre,auxPuntuacion,auxRanking,auxFecha);
     }
     
 }
