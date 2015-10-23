d5d9e055850c2536fb18905e86bede7970761c70
Raúl Ibáñez <raul_ibpe@hotmail.com>
2015-10-23 12:01:19 +0200

    Update ControladorPersistencia.java

--- Persistencia/ControladorPersistencia.java
+++ Persistencia/ControladorPersistencia.java
@@ -14,20 +14,6 @@ public class ControladorPersistencia {
     */
     public ControladorPersistencia() {
         
-    } 
-    
-    /*  PRE: -
-        POST: Se retorna true si el usuario existe en la base de datos, false en 
-        caso contrario
-    */
-    public boolean iniciar_sesion(String nombre, String contrasena) {
-        return true;
-        /* AQUÍ SE HACE EL PROCESO NECESARIO PARA COMPROBAR EL USUARIO EN LA BASE DE DATOS */
-    }
-    
-    public boolean nombreTableroValido(String s) {
-        return true;
-        /* AQUÍ SE HACE EL PROCESO NECESARIO PARA COMPROBAR EL USUARIO EN LA BASE DE DATOS */
     }
     
     public boolean nombreUsuarioExistente(String s){
