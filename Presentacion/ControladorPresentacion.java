c8e34875bffec7f8012594cb80240752dedadf93
Raúl Ibáñez <raul_ibpe@hotmail.com>
2015-10-23 13:03:54 +0200

    Update ControladorPresentacion.java

--- Presentacion/ControladorPresentacion.java
+++ Presentacion/ControladorPresentacion.java
@@ -79,27 +79,31 @@ public class ControladorPresentacion {
         POST: Se lee la opción que el jugador escoge
     */
     private int introducirOpcion() {
-	int opcion;
+	
 	System.out.println("    ############################");
         System.out.println("    ####   MENÚ PRINCIPAL   ####");
         System.out.println("    ############################\n");
+        System.out.println("    Datos del usuario:");
         if(this.usuarioInvitado) 
-            System.out.println("    Usuario invitado\n\n");
+            System.out.println("    > Usuario invitado\n");
         else{
-            System.out.println("    Usuario: "+nombreUsuario+"\n");
-            System.out.println("    Puntuación Total: "+puntuacionTotal+"\n");
+            System.out.println("    > Usuario: "+nombreUsuario+"");
+            System.out.println("    > Puntuación Total: "+puntuacionTotal);
             if (CDominio.getApareceRanking())
-                System.out.println("    Sí aparece en Ránkings\n");
-            else System.out.println("    No aparece en Ránkings\n");
-            System.out.println("    Fecha Creación: "+fechaCreacion+"\n\n");
+                System.out.println("    > Sí aparece en Ránkings");
+            else System.out.println("    > No aparece en Ránkings");
+            System.out.println("    > Fecha Creación: "+fechaCreacion+"\n\n");
         }
-	System.out.println("    Escoje opción:\n\n");
-        System.out.println("        1) Gestión de Usuario\n");
-	System.out.println("        2) Jugar\n");
-	System.out.println("        3) Repositorio de Hidatos\n");
-        System.out.println("        4) Rankings y Récords\n\n");
-        System.out.println("        5) Salir\n");
-        System.out.print("\n      > opción:  ");
+        
+        int opcion;
+        
+	System.out.println("    Escoje opción:");
+        System.out.println("    #1) Gestión de Usuario");
+	System.out.println("    #2) Jugar");
+	System.out.println("    #3) Repositorio de Hidatos");
+        System.out.println("    #4) Rankings y Récords");
+        System.out.println("    #5) Salir\n");
+        System.out.print("    > opción:  ");
 	Scanner in = new Scanner(System.in);
 	opcion = in.nextInt(); //lectura de datos
         System.out.print("\n");
@@ -132,7 +136,7 @@ public class ControladorPresentacion {
             Scanner in = new Scanner(System.in);
             nombreUsuario = in.nextLine();
             if (CDominio.nombreExistente(nombreUsuario) == false) {
-                System.out.println("\n  Ese nombre de usuario no existe.\n");
+                System.out.println("\n    Ese nombre de usuario no existe.\n");
                 opcionInvalida = true;
                 if (!salirOpcion) {
                     while (opcionInvalida) {
@@ -145,26 +149,26 @@ public class ControladorPresentacion {
                             opcionInvalida = false;
                         }
                         else if (salirString.equals("S") || salirString.equals("s")){
-                            System.out.println("\n  Introduce tu nombre de usuario:\n");
+                            System.out.println("\n    Introduce tu nombre de usuario:\n");
                             opcionInvalida = false;
                         }
                         else 
-                            System.out.println("\n  Solo puedes usar S / N. Vuelve a intentarlo.\n");
+                            System.out.println("\n    Solo puedes usar S / N. Vuelve a intentarlo.\n");
                     }
                 }
             }
             else nombreCompletado = true;
         }
         
-        System.out.println("\n  Nombre de usuario válido.\n");
-        System.out.println ("   Introduce tu contraseña:\n");
+        System.out.println("\n    Nombre de usuario válido.\n");
+        System.out.println ("    Introduce tu contraseña:\n");
         
         while (!contrasenaCompletada && !salirOpcion) {
             System.out.print("      > contraseña: ");
             Scanner in = new Scanner(System.in);
             nombreContrasena = in.nextLine();
             if (CDominio.contrasenaValida(nombreUsuario,nombreContrasena) == false) {
-                System.out.println("\n  La contraseña no es correcta.\n");
+                System.out.println("\n    La contraseña no es correcta.\n");
                 opcionInvalida = true;
                 if (!salirOpcion) {
                     while (opcionInvalida) {
@@ -177,11 +181,11 @@ public class ControladorPresentacion {
                             opcionInvalida = false;
                         }
                         else if (salirString.equals("S") || salirString.equals("s")){
-                            System.out.println("\n  Introduce tu contraseña:\n");
+                            System.out.println("\n    Introduce tu contraseña:\n");
                             opcionInvalida = false;
                         }
                         else 
-                            System.out.println("\n  Solo puedes usar S / N. Vuelve a intentarlo.\n");
+                            System.out.println("\n    Solo puedes usar S / N. Vuelve a intentarlo.\n");
                     }
                 }
             }
@@ -190,13 +194,13 @@ public class ControladorPresentacion {
         
         if (salirOpcion == false){
             CDominio.loginUsuario(nombreUsuario, nombreContrasena);
-            System.out.println("\n  Contraseña válida, usuario logueado.\n");
+            System.out.println("\n    Contraseña válida, usuario logueado.\n");
             ControladorPresentacion.usuarioInvitado = false;
             ControladorPresentacion.nombreUsuario = nombreUsuario;
             ControladorPresentacion.puntuacionTotal = CDominio.getPuntuacionUsuario();
             ControladorPresentacion.fechaCreacion = CDominio.getFechaCreacion();
         }
-        else System.out.println("\n Usuario NO logueado.\n");
+        else System.out.println("\n    Usuario NO logueado.\n");
         
         System.out.println("    Volviendo al Menú Principal.\n");
         
@@ -227,14 +231,14 @@ public class ControladorPresentacion {
         String nombreUsuario = "";
         String nombreContrasena = "";
         
-        System.out.println("    Introduce un nombre de usuario: (6 o más caracteres)\n");
+        System.out.println("    Introduce un nombre de usuario: (6 o más caracteres alfanuméricos)\n");
         
         while (!nombreCompletado && !salirOpcion) {
             System.out.print("      > usuario: ");
             Scanner in = new Scanner(System.in);
             nombreUsuario = in.nextLine();
             if (CDominio.nombreValido(nombreUsuario) == false) {
-                System.out.println("\n  Ese nombre de usuario ya existe o es inválido.\n");
+                System.out.println("\n    Ese nombre de usuario ya existe o es inválido.\n");
                 opcionInvalida = true;
                 if (!salirOpcion) {
                     while (opcionInvalida) {
@@ -247,26 +251,26 @@ public class ControladorPresentacion {
                             opcionInvalida = false;
                         }
                         else if (salirString.equals("S") || salirString.equals("s")){
-                            System.out.println("\n  Elige otro nombre de usuario:\n");
+                            System.out.println("\n    Elige otro nombre de usuario:\n");
                             opcionInvalida = false;
                         }
                         else 
-                            System.out.println("\n  Solo puedes usar S / N. Vuelve a intentarlo.\n");
+                            System.out.println("\n    Solo puedes usar S / N. Vuelve a intentarlo.\n");
                     }
                 }
             }
             else nombreCompletado = true;
         }
         
-        System.out.println("\n  Nombre de usuario válido.");
-        System.out.println("    Introduce una contraseña: (6 o más caracteres)\n");
+        System.out.println("\n    Nombre de usuario válido.");
+        System.out.println("    Introduce una contraseña: (6 o más caracteres alfanuméricos)\n");
         
         while (!contrasenaCompletada && !salirOpcion) {
             System.out.print("      > contraseña: ");
             Scanner in = new Scanner(System.in);
             nombreContrasena = in.nextLine();
             if (CDominio.contrasenaValida(nombreContrasena)) {
-                System.out.println("\n  Esa contraseña no es válida.\n");
+                System.out.println("\n    Esa contraseña no es válida.\n");
                 opcionInvalida = true;
                 if (!salirOpcion) {
                     while (opcionInvalida) {
@@ -279,11 +283,11 @@ public class ControladorPresentacion {
                             opcionInvalida = false;
                         }
                         else if (salirString.equals("S") || salirString.equals("s")){
-                            System.out.println("\n  Elige otra contraseña:\n");
+                            System.out.println("\n    Elige otra contraseña:\n");
                             opcionInvalida = false;
                         }
                         else 
-                            System.out.println("\n  Solo puedes usar S / N. Vuelve a intentarlo.\n");
+                            System.out.println("\n    Solo puedes usar S / N. Vuelve a intentarlo.\n");
                     }
                 }
             }
@@ -292,12 +296,12 @@ public class ControladorPresentacion {
         
         if (salirOpcion == false){
             CDominio.crearUsuario(nombreUsuario, nombreContrasena);
-            System.out.println("\n  Contraseña válida, usuario Creado.\n");
+            System.out.println("\n    Contraseña válida, usuario Creado.\n");
             ControladorPresentacion.usuarioInvitado = false;
             ControladorPresentacion.nombreUsuario = nombreUsuario;
             ControladorPresentacion.puntuacionTotal = CDominio.getPuntuacionUsuario();
         }
-        else System.out.println("\n Usuario NO creado.\n");
+        else System.out.println("\n    Usuario NO creado.\n");
         
         System.out.println("    Volviendo al Menú Principal.\n");
         
